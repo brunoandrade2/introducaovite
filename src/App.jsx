571ee59085a4d485f3235ee0d81ebc6f2d964b51
brunoandrade2 <ndbs11118@gmail.com>
2026-02-24 11:18:35 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App () {
+    return(
+      <div>
+        <h1>Aula de React com Vite JS  </h1>
+        <h2>Estilzação CSS</h2>
+        <p>eu nao fiz nada</p>
+      </div>
+
+    )
+  }
+
+  export default App;
